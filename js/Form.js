c01db71836b924c380902fc0d794b7bc6f774c2a
darshankg2020 <darshankumarkg2020@gmail.com>
2021-09-27 17:11:34 +0530

    learned about html elements

--- js/Form.js
+++ js/Form.js
@@ -12,4 +12,38 @@ class Form {
     this.input.hide();
   }
 
+  setElementPosition(){
+    this.titleImg.position(120,160);
+    this.input.position(width/2-110,height/2-80);
+    this.playButton.position(width/2-90,height/2-20);
+    this.greeting.position(width/2-300,height/2-100);
+  }
+
+  setElementStyle(){
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  handleMousePressed(){
+    this.playButton.mousePressed(()=>{
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Hello ${this.input.value()}
+      </br>wait for another player to join...`;
+      this.greeting.html(message);
+    })
+
+  }
+
+  display(){
+    this.setElementPosition();
+    this.setElementStyle();
+    this.handleMousePressed();
+
+  }
+
 }
+
